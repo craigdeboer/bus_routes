860f80bfc0d1197c91c5ed691bfd9fd214b81534
Craig Deboer <craig@cdeboer.net>
2015-07-24 15:01:54 -0700

    Add ability to display student addresses on a google map

--- app/assets/javascripts/manage_students.js
+++ app/assets/javascripts/manage_students.js
@@ -30,3 +30,56 @@ busRoutesApp.controller('StudentsListCtrl', function ($scope, Student) {
     $scope.formDisplay = false;
   };
 });
+
+busRoutesApp.controller('MapCtrl', function ($scope, Student) {
+// If I just perform the Student.query() and then execute 
+// the setMarkers function, it fails because I'm trying to access
+// the $scope.students before the query is completed.
+// If I provide a function within the query, and call the
+// setMarkers function within that callback function, I ensure the
+// query is complete before I try to access the data.  
+  var students = Student.query(function() {
+    $scope.students = students;
+    setMarkers();
+  });
+  $scope.testCase = [];
+  function initialize() {
+    var mapOptions = {
+      center: { lat: 49.1387, lng: -122.8218 },
+      zoom: 13
+    };
+    $scope.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(49.170385, -122.796939),
+      map: $scope.map,
+      label: 'A',
+      icon: '/assets/blue_MarkerA.png',
+      animation: google.maps.Animation.BOUNCE,
+      title: "Surrey High"
+    });
+  };
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  setMarkers = function () {
+    angular.forEach($scope.students, function(student) {
+     $scope.testCase.push(student.latitude);
+     studentLatLng = new google.maps.LatLng(student.latitude, student.longitude);
+     marker = new google.maps.Marker({
+       position: studentLatLng,
+       map: $scope.map,
+       title: student.first_name + student.last_name
+     });
+    });
+  };   
+});
+
+
+
+
+
+
+
+
+
+
+
