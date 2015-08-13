6ac89f564121e3fc55d31b055851699b4259b453
Craig Deboer <craig@cdeboer.net>
2015-08-13 12:01:56 -0700

    Implement successful import of standard students

--- app/controllers/import_students_controller.rb
+++ app/controllers/import_students_controller.rb
@@ -7,5 +7,8 @@ class ImportStudentsController < ApplicationController
   end
 
   def standard_import
+    Student.standard_import(params[:file])
+    flash[:success] = "Students Imported"
+    redirect_to new_import_path
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,6 +13,25 @@ class Student < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode, if: :street_address_changed?
 
+  def self.standard_import(file)
+    spreadsheet = Roo::Excelx.new(file.path)
+    (1..spreadsheet.last_row).each do |row|
+      first_name = spreadsheet.cell(row, 1)
+      last_name = spreadsheet.cell(row, 2)
+      school = spreadsheet.cell(row, 3)
+      grade = spreadsheet.cell(row, 4)
+      phone = spreadsheet.cell(row, 5)
+      email = spreadsheet.cell(row, 6)
+      street_address = spreadsheet.cell(row, 7)
+      city = spreadsheet.cell(row, 8)
+      postal_code = spreadsheet.cell(row, 9)
+      bus_route = spreadsheet.cell(row, 10)
+      student = Student.new(first_name: first_name, last_name: last_name, school: school, 
+                            grade: grade, phone: phone, email: email, street_address: street_address, 
+                            city: city, postal_code: postal_code, bus_route: bus_route)
+      student.save!
+    end
+  end
   private
 
   def full_address
