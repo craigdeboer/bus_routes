92e383b5b601eb048fd2acc1c7e2fdaa26ed28be
Craig Deboer <craig@cdeboer.net>
2015-08-25 16:01:11 -0700

    Add authentication for the ManageStudents controller

--- app/controllers/manage_students_controller.rb
+++ app/controllers/manage_students_controller.rb
@@ -1,4 +1,19 @@
 class ManageStudentsController < ApplicationController
+  before_action :check_for_admin
   def home
   end
+
+  private
+
+  def check_for_admin
+    admin_users = ["doug@ccsta.net", "debbie@ccsta.net", "craig@cdeboer.net"]
+    if !user_signed_in?
+      flash[:notice] = "You must be logged in to access the requested page."
+      redirect_to root_path
+    elsif !admin_users.include?(current_user.email)
+      flash[:notice] = "You must be an admin user to access the requested page."
+      redirect_to root_path 
+    end
+  end
+      
 end
