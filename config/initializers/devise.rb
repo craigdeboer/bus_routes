9c7b9abfeaf7f3a05e1c07847941efdbc6da6874
Craig Deboer <craig@cdeboer.net>
2015-09-08 11:00:36 -0700

    Set devise secret key to the secret key environment variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,8 +6,8 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = ''
-  # ==> Mailer Configuration
+  config.secret_key = <%= ENV["SECRET_KEY_BASE"] %>
+  
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
